62a4d022ec7d1fc5022f03d00d832ff3cf88576f
nobody <github2@invisiblehand.church>
2025-03-04 07:58:00 +0000

    x

--- lib/AI/Msg.pm
+++ lib/AI/Msg.pm
@@ -28,50 +28,6 @@ $columns=55;
     };
   }
 };
-{
-  package DblTie;
-  use Tie::Hash;
-  our(@ISA) = qw(Tie::Hash);
-  our(%val) = (
-    role=>1,
-    name=>2,
-    text=>4
-  );
-  sub TIEHASH {
-    my ( $class ) = map { ref || $_ } shift;
-    die "Created without inits" unless @_==3;
-    my ( $self ) = {};
-    $self->{role}=shift;
-    $self->{name}=shift;
-    $self->{text}=shift;
-    die "null(s)" if (grep { !defined } %val);
-    return bless($self,$class);
-  }
-  sub FETCH {
-    my $self=shift;
-    my $key=shift;
-    die "bad key" unless $val{$key};
-    my $val=$self->{$key};
-    die "val is null" unless defined $val;
-    $val;
-  }
-  sub STORE {
-    my $self=shift;
-    my $key=shift;
-    my $val=shift;
-    die "bad key" unless $val{$key};
-    die "$key is null" unless defined $val;
-    if( $key eq "text" ) {
-      $val=wrap( "","", "$val" );
-    };
-    $self->{$key}=$val;
-  };
-};
-sub mk_self() {
-  my (%hash);
-  tie %hash, 'DblTie';
-  return \%hash;
-};
 sub new {
     my ($class, $role, $name, $text) = @_;
     $_ = (ref || $_) for $class;
@@ -89,14 +45,8 @@ sub new {
     };
     warn "$text is a ref" if ref($text);
     warn "$text is an existing filesystem object" if -e $text;
-    my $self = mk_self();
-    die "$text is a ref" if ref($text);
-    die "$text is an existing filesystem object" if -e $text;
+    my $self = { role=>$role, name=>$name, text=>$text };
     $text=wrap("","",$text);
-    my $self = {};
-    $self->{role}=$role;
-    $self->{text}=wrap("","",$text);
-    $self->{name}=$name;
     bless $self, $class;
 }
 
